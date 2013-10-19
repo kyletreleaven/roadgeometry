1c93c44b2ec2704965015c7b1dc3e537108e6d69
Kyle Treleaven <treleaven.kyle@gmail.com>
2013-10-19 08:04:11 -0400

    packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+	name = "roadgeometry",
+	description = "A small library for road network geometry queries",
+	author = "Kyle Treleaven",
+	author_email = "ktreleav@gmail.com",
+	version = "0.0.0",
+	packages = find_packages(),
+	namespace_packages = [ 'setiptah', ],
+)
+
