7da42058f3c9d759317787e7a087cd6028b71a38
Kyle Treleaven <ktreleav@gmail.com>
2025-09-06 11:13:00 -0400

    tdd: how can there be a bug in old distance? did i introduce one?

--- setiptah-roadgeometry/src/setiptah/roadgeometry/roadmap_paths.py
+++ setiptah-roadgeometry/src/setiptah/roadgeometry/roadmap_paths.py
@@ -1,19 +1,5 @@
-import logging
-from dataclasses import dataclass
-from typing import Any
-
-import networkx as nx
-import numpy as np
-
-from . import astar_basic
-from .roadmap_basic import RoadAddress
-from .roadmap_basic import check_point, obtain_edge
-
-LOG = logging.getLogger(__name__)
-
-__author__ = "Kyle Treleaven <ktreleav@gmail.com>"
-
 """
+
 Notes on the Roadmap format:
 These utilities expect a NetworkX MultiDiGraph,
 the edge keys are interpreted as "road names",
@@ -21,12 +7,22 @@ and they should all be unique, even among edges between different nodes;
 (also, they should be distinct from the node labels).
 All roads are two-way by default (the direction of edges is for the coordinate system only);
 However, one-way roads are handled, and should be indicated with a key:value pair 'oneway':True .
-"""
-
 
 """
-Class defs --- for paths
-"""
+import logging
+from dataclasses import dataclass
+from typing import Any
+
+import networkx as nx
+import numpy as np
+
+from .roadmap_basic import RoadAddress
+from .roadmap_basic import check_point
+
+LOG = logging.getLogger(__name__)
+
+__author__ = "Kyle Treleaven <ktreleav@gmail.com>"
+
 
 @dataclass(frozen=True, repr=False)
 class RoadSegment(object):
@@ -36,32 +32,18 @@ class RoadSegment(object):
 
     def __repr__(self) :
         return '(%s,[%s,%s])' % ( repr(self.road), repr(self.first), repr(self.second) )
-    
-    
-def check_segment( segment, roadmap, length_attr='length' ) :
-    # a rare case where "static variable" is useful;
-    # to prevent "costly" re-allocation of RoadAddress structure
-    if not hasattr(check_segment, 'first' ) :
-        check_segment.first = RoadAddress(None,None)
-    if not hasattr(check_segment, 'second' ) :
-        check_segment.second = RoadAddress(None,None)
-        
-    road = segment.road
-    check_segment.first.road = road
-    check_segment.first.coord = segment.first
-    check_segment.second.road = road
-    check_segment.second.coord = segment.second
-    
+
+
+def check_segment(
+        segment: RoadSegment, roadmap: nx.MultiDiGraph, length_attr: str ='length'
+):
     try :
-        assert check_point( roadmap, check_segment.first, length_attr )
-        assert check_point( roadmap, check_segment.second, length_attr )
+        assert check_point(roadmap, RoadAddress(segment.road, segment.first), length_attr)
+        assert check_point(roadmap, RoadAddress(segment.road, segment.second), length_attr)
     except AssertionError :
         return False
-    
     return True
 
-
-""" Simple Path Operations """
 
 def pathReversed( path ) :
     res = []
@@ -69,12 +51,14 @@ def pathReversed( path ) :
         res.insert(0, RoadSegment( segment.road, segment.second, segment.first ) )
     return res
 
+
 def pathLength( path ) :
     if len( path ) <= 0 :
         return np.inf
     else :
         segLengths = [ np.abs( seg.first - seg.second ) for seg in path ]
         return np.sum( segLengths )
+
 
 def pathEvaluate( path, x ) :
     if x < 0. : raise Exception('cannot interpret negative distance')
@@ -94,152 +78,23 @@ def pathEvaluate( path, x ) :
         return RoadAddress( segment.road, segment.first + dx )
     else :
         return RoadAddress( segment.road, segment.first - dx )
-    
-def pathFromAStar( astar_path, roadmap, length_attr ) :
-    path = []
-    
-    nodewalk = astar_path[::2]
-    edgewalk = astar_path[1::2]
-    for i,j, edgedata in zip( nodewalk[:-1], nodewalk[1:], edgewalk ) :
-        _,__,road = edgedata
-        edge, data  = obtain_edge( roadmap, road, True )
-        u,v,_       = edge
-        roadlen     = data.get( length_attr, 1 )
-        
-        if i == u and j == v :  # forward jump, yay!
-            segment = RoadSegment( road, 0., roadlen )
-        elif i == v and j == u : # backward jump, yay!
-            segment = RoadSegment( road, roadlen, 0. )
-        else :
-            raise Exception('invalid hop on roadmap')
-        
-        path.append( segment )
-    
-    return path
 
-
-""" Shortest Path Functions """
 
 def minpath(
         p: RoadAddress, q: RoadAddress, roadmap: nx.MultiDiGraph, length_attr: str = "length"
 ):
     """ returns one of the min length paths between two points on a Roadmap """
-    # some convenient static variables
 
     from setiptah.roadbm.nx_legacy import MultiDiGraphRoadnet
     from setiptah.basic_graph.dijkstra import RoadnetMetric
+    metric = RoadnetMetric(MultiDiGraphRoadnet(roadmap, length_attr=length_attr))
 
     p_ = p.road, p.coord
     q_ = q.road, q.coord
 
-    metric = RoadnetMetric(MultiDiGraphRoadnet(roadmap, length_attr=length_attr))
-    # assert False, metric._shortest_path(p_, q_)._result
-
     path_ = metric.shortest_path(p_, q_)
-
     return [RoadSegment(seg.road, seg.start, seg.end) for seg in path_]
 
-
-
-
-
-    if not hasattr( minpath, 'STATICS' ) :
-        minpath.STATICS = True
-        minpath.first = RoadAddress(None,None)
-        minpath.second = RoadAddress(None,None)
-        minpath.points = [ minpath.first, minpath.second ]
-    
-    # convenient non-static variables
-    road        = p.road
-    edge, data  = obtain_edge( roadmap, road, True )
-    i,j,key     = edge
-    roadlen     = data.get( length_attr, 1 )
-    
-    # we will be choosing best path among options
-    options = []
-    
-    # one possible type of path is a segment on a single road
-    path = minpath_on_road( p, q, road, roadmap, length_attr )
-    options.append( ( pathLength(path), path ) )
-    
-    # two more possible types of paths are p->u->q, for u either of endpoints of road of p
-    minpath.first.init(road,0.)
-    minpath.second.init(road,roadlen)
-    for u, qq in zip( (i,j), minpath.points ) :
-        first = minpath_on_road( p, qq, road, roadmap, length_attr )
-        second = minpath_node_to_point( u, q, roadmap, length_attr )
-        path = first + second
-        options.append( ( pathLength(path), path ) )
-        
-    # return path having the shortest length --- might be []
-    return min( options )[1]
-        
-
-def minpath_on_road( p, q, road, roadmap, length_attr='length' ) :
-    """
-    subroutine and useful utility:
-    returns the path from p to q on road, if the direction of travel is admissible;
-    if not, or if p and q are not co-'road'-al, returns empty path, implying non-existence 
-    """ 
-    edge, data  = obtain_edge( roadmap, road, True )
-    i,j,key     = edge
-    
-    path = []
-    # regular logic, NOT error catching
-    try :
-        assert p.road == road and check_point( roadmap, p, length_attr )
-        assert q.road == road and check_point( roadmap, q, length_attr )
-        assert p.coord < q.coord or not data.get( 'oneway', False )
-        path.append( RoadSegment( road, p.coord, q.coord ) )
-        
-    except AssertionError :
-        pass
-    
-    return path
-
-
-def minpath_node_to_point( u, q, roadmap, length_attr='length' ) :
-    """
-    subroutine and useful utility:
-    returns the shortest-path distance from a node u to a point q, on digraph    
-    """
-    
-    # some convenient static variables
-    scope = minpath_node_to_point
-    if not hasattr( scope, 'STATICS' ) :
-        scope.STATICS = True
-        scope.first = RoadAddress(None,None)
-        scope.second = RoadAddress(None,None)
-        scope.points = [ scope.first, scope.second ]
-    
-    # convenient non-static variables
-    assert check_point( roadmap, q, length_attr )
-    road        = q.road
-    edge, data  = obtain_edge( roadmap, road, True )
-    i,j,key     = edge
-    roadlen = data.get( length_attr, 1 )
-    
-    # pre-load options with nopath; in case there are no feasible paths
-    options = [ (np.inf, [] ) ]
-    
-    # possible paths are u->v->q, for v in endpoints of road of q
-    scope.first.init(road,0.)
-    scope.second.init(road,roadlen)
-    for v, pp in zip( (i,j), scope.points ) :
-        try :
-            astarPath = astar_basic.astar_path( roadmap, u, v, None, weight=length_attr )
-            first = pathFromAStar( astarPath, roadmap, length_attr )
-        except nx.NetworkXNoPath :
-            continue
-        second = minpath_on_road( pp, q, road, roadmap, length_attr )
-        path = first + second
-        options.append( (pathLength(path), path ) )
-        
-    return min( options )[1]
-
-
-
-""" Compound Path Operations """
 
 def pathExtend( path, nextAddress, roadmap, length_attr='length' ) :
     segment = path[-1]
@@ -251,60 +106,9 @@ def pathExtend( path, nextAddress, roadmap, length_attr='length' ) :
     return path + second
 
 
-""" Path Planning """
-
 class RoadTrajectory :
     def __init__(self, path ) :
         self.path = path
         
     def __call__(self, progress ) :
         return pathEvaluate( self.path, progress )
-    
-    
-class RoadmapPlanner :
-    def __init__(self, roadmap ) :
-        self.roadmap = roadmap
-        
-    def __call__(self, orig, dest, length_attr='length' ) :
-        path = minpath( orig, dest, self.roadmap, length_attr )
-        return pathLength(path), RoadTrajectory(path)
-
-
-
-
-if __name__ == '__main__' :
-    from setiptah.roadgeometry.roadmap_basic import distance
-    
-    import setiptah.roadgeometry.probability as roadprob
-    
-    if True :
-        roadmap = roadprob.sampleroadnet()
-        p = roadprob.sampleaddress( roadmap )
-        q = roadprob.sampleaddress( roadmap )
-    
-    # give me a summary of the road network
-    for u,v,road, data in roadmap.edges( keys=True, data=True ) :
-        LOG.debug('%s: %s -> %s ; length=%f' % ( road, repr(u), repr(v), data.get('length',1) ))
-        
-    LOG.debug('...going from %s to %s' % ( repr(p), repr(q) ))
-    frwd = minpath( p, q, roadmap )
-    frwdL = pathLength( frwd )
-    frwdLRef = distance( roadmap, p, q, 'length' )
-    LOG.debug(frwd)
-    LOG.debug('FRWD LENGTH: %f; survey says: %f' % ( frwdL, frwdLRef ))
-    
-    
-    bkwd = minpath( q, p, roadmap )
-    bkwdL = pathLength( bkwd )
-    bkwdLRef = distance( roadmap, q, p, 'length' )
-    
-    LOG.debug('BKWD LENGTH: %f; survey says: %f' % ( bkwdL, bkwdLRef ))
-    
-    if np.abs( frwdL - frwdLRef ) > 10**-10 or np.abs( bkwdL - bkwdLRef ) > 10**-10 :
-        LOG.debug('ALERT!!! NOT AGREE WITH REF')
-    else :
-        LOG.debug('cool as a cucumber')
-        
-        
-        
-        
--- setiptah-roadgeometry/test/test_roadmap_paths.py
+++ setiptah-roadgeometry/test/test_roadmap_paths.py
@@ -3,6 +3,13 @@ import networkx as nx
 from setiptah.roadgeometry.roadmap_paths import *
 from setiptah.roadgeometry.roadmap_basic import RoadAddress
 
+import logging
+
+import pytest
+
+LOG = logging.getLogger(__name__)
+
+
 def test_minpath():
 
     g = nx.MultiDiGraph()
@@ -13,3 +20,96 @@ def test_minpath():
     q = RoadAddress("R", 9)
 
     assert minpath(p, q, g) == [RoadSegment("R", 1, 9)]
+
+
+@pytest.mark.skip
+def test_pathlen():
+    from setiptah.roadgeometry.roadmap_basic import distance
+
+    import setiptah.roadgeometry.probability as roadprob
+
+    roadmap = roadprob.sampleroadnet()
+
+    p = roadprob.sampleaddress(roadmap)
+    q = roadprob.sampleaddress(roadmap)
+
+    frwd = minpath(p, q, roadmap)
+    frwdL = pathLength(frwd)
+    frwdLRef = distance(roadmap, p, q, 'length')
+
+    if np.abs(frwdL - frwdLRef) < 10 ** -10:
+        pass
+
+    else:
+
+        with open("pathlen-error.txt", "w") as f:
+
+            def writeline(line):
+                f.write(f"{line}\n")
+
+            # writeline("from setiptah.basic_graph.graphs import RoadNetwork")
+            # writeline("from setiptah.basic_graph.dijkstra import *")
+            writeline("import networkx as nx")
+            writeline("from setiptah.roadgeometry.roadmap_basic import *")
+            writeline("from setiptah.roadgeometry.roadmap_paths import *")
+            # writeline("rn = RoadNetwork()")
+            writeline("rn = nx.MultiDiGraph()")
+            for u in roadmap.nodes():
+                writeline(f"rn.add_node({u})")
+
+            for u, v, road, data in roadmap.edges(keys=True, data=True):
+                l = data.get("length", 1)
+                # writeline(f'rn.add_edge("{road}", {u}, {v}, {l})')
+                writeline(f'rn.add_edge({u}, {v}, "{road}", length={l})')
+
+                if False:
+                    writeline(
+                        '%s: %s -> %s ; length=%f' % (road, repr(u), repr(v), data.get('length', 1))
+                    )
+
+            writeline(f'p = RoadAddress("{p.road}", {p.coord})')
+            writeline(f'q = RoadAddress("{q.road}", {q.coord})')
+
+        assert False
+
+
+def test_canned():
+    import networkx as nx
+
+    from setiptah.roadgeometry.roadmap_basic import distance
+    from setiptah.roadgeometry.roadmap_paths import minpath
+
+    rn = nx.MultiDiGraph()
+    rn.add_node(0)
+    rn.add_node(2)
+    rn.add_node(1)
+    rn.add_node(3)
+    rn.add_node(5)
+    rn.add_node(6)
+    rn.add_node(7)
+    rn.add_node(8)
+    rn.add_node(4)
+    rn.add_node(9)
+    rn.add_edge(0, 2, "road0", length=0.08690718896367763)
+    rn.add_edge(2, 7, "road6", length=1.0090526344825517)
+    rn.add_edge(1, 3, "road1", length=0.3581567968145343)
+    rn.add_edge(1, 5, "road2", length=3.0405091619281603)
+    rn.add_edge(1, 6, "road3", length=0.5038404955090379)
+    rn.add_edge(1, 7, "road4", length=1.9846761312373984)
+    rn.add_edge(1, 8, "road5", length=0.16952917209609902)
+    rn.add_edge(3, 4, "road7", length=0.30725919265278534)
+    rn.add_edge(3, 5, "road8", length=1.1650558623014722)
+    rn.add_edge(5, 6, "road9", length=0.2031866604566163)
+    rn.add_edge(5, 7, "road10", length=1.3770561219375455)
+    rn.add_edge(5, 8, "road11", length=0.1971187509079228)
+    rn.add_edge(7, 8, "road12", length=0.1948755750344511)
+    rn.add_edge(7, 9, "road13", length=0.6278788902822177)
+
+    p = RoadAddress("road7", 0.16554418185170827)
+    q = RoadAddress("road4", 0.6748362063998169)
+
+    frwd = minpath(p, q, rn)
+    frwdL = pathLength(frwd)
+    frwdLRef = distance(rn, p, q, 'length')
+
+    assert frwdLRef < np.inf
