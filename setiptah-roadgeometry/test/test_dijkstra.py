7d282a74582c8cc0cf6b8fcbcaf0349fac4d920e
Kyle Treleaven <ktreleav@gmail.com>
2025-09-11 16:19:01 -0400

    pulling mygraph shortest path test into matching

--- setiptah-roadgeometry/test/test_dijkstra.py
+++ setiptah-roadgeometry/test/test_dijkstra.py
@@ -1,5 +1,3 @@
-import itertools
-
 import networkx as nx
 import pytest
 
@@ -9,44 +7,6 @@ from setiptah.roadgeometry.graphs import RoadNetwork
 from setiptah.roadgeometry.legacy.conversion import create_multigraph
 from setiptah.roadgeometry.matching.nx_legacy import MultiDiGraphRoadnet
 from setiptah.roadgeometry.matching.util.mygraph import *
-
-
-def test_dijkstra():
-    g = mygraph()
-    g.add_edge('a', 0, 1)
-    g.add_edge('b', 0, 2)
-    g.add_edge('c', 2, 3)
-    g.add_edge('d', 3, 0)
-
-    c = {'a': 10., 'b': 5., 'c': 1., 'd': .5}
-    f = {'a': 3., 'b': 1.37}
-
-    g = mygraph()
-    c = {}
-    edgegen = itertools.count()
-
-    nxg = nx.DiGraph()
-
-    X = [(i, np.random.rand(2)) for i in range(10)]
-    for (i, x), (j, y) in itertools.product(X, X):
-        cost = np.linalg.norm(y - x)
-        if cost < .3:
-            e = next(edgegen)
-            g.add_edge(e, i, j)
-            c[e] = cost
-
-            nxg.add_edge(i, j, weight=cost)
-
-    s = 0
-    d, pred = Dijkstra(g, c, s)
-
-    # Compare to networkx solution
-    nxd = nx.single_source_dijkstra_path_length(nxg, s)
-
-    assert all(
-        d.get(u) == nxd.get(u)
-        for u in set(d) | set(nxd)
-    )
 
 
 @pytest.fixture
