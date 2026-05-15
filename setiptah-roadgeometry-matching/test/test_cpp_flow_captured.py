c3e20c2ddf82c80e9d4ab0c1a62335c63dfffe2d
Kyle Treleaven <ktreleav@gmail.com>
2026-05-15 06:39:17 -0400

    don't be silly

--- setiptah-roadgeometry-matching/test/test_cpp_flow_captured.py
+++ setiptah-roadgeometry-matching/test/test_cpp_flow_captured.py
@@ -54,7 +54,7 @@ def test_cpp_optimal_flow_matches_python(roadnet):
 
     from setiptah.roadgeometry.matching.io import load_pins
     from setiptah.roadgeometry.matching.bm import (
-        PREMATCH, MEASURE, OBJECTIVE_FUNC,
+        MEASURE, OBJECTIVE_FUNC,
         compute_segments2, flow_from_segments,
     )
     from setiptah.roadgeometry.matching.nxopt.cvxcostflow import MinConvexCostFlow
@@ -63,15 +63,11 @@ def test_cpp_optimal_flow_matches_python(roadnet):
 
     # --- cpp path (under test) ---
     seg_cpp = _cpp.sort_and_segment(P, Q, list(roadnet.edges()))
-    for seg in seg_cpp.values():
-        PREMATCH(seg)
     endpoints, lengths, is_oneway = roadnet.graph_props(seg_cpp)
     flow_cpp = _cpp.compute_optimal_flow(seg_cpp, endpoints, lengths, is_oneway)
 
     # --- python path (reference) ---
     seg_py = compute_segments2(P, Q, roadnet)
-    for seg in seg_py.values():
-        PREMATCH(seg)
     flow_py = flow_from_segments(seg_py, roadnet, flow_solver=MinConvexCostFlow)
 
     obj = {
